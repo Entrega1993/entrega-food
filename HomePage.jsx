940cab376c317a6106a0fe840bbc343c21aa8dde
Entrega1993 <juan@conceptos.ca>
2025-05-10 16:23:32 -0400

    Add files via upload

---
+++ HomePage.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function HomePage() {
+  return (
+    <div className="p-6 text-center">
+      <h1 className="text-4xl font-bold mb-4">Welcome to Entrega.food</h1>
+      <p className="text-lg">This is your custom food delivery website.</p>
+    </div>
+  );
+}
